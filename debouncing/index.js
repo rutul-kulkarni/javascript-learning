2aa478b161f2f11a934f4b41035b707489a5e6d7
rutul-kulkarni <rutulkulkarni8380@gmail.com>
2023-05-23 16:21:43 +0530

    explaination added in debouncing

--- debouncing/index.js
+++ debouncing/index.js
@@ -1,3 +1,8 @@
+/* 
+It is used to limit the expensive function call. 
+Imagine a case of search functionality. on every key press there are chances of api fire. so we can limit this api call via debouncing
+*/
+
 let functionCounter = 0;
 
 function fetchData() {
@@ -8,11 +13,15 @@ function debounceAction(fn, timeDiff) {
   let timer;
 
   return function () {
+    // this will have context of calling function and arguemnts will have arguments passed to called function ie. debouncefunc
     let context = this,
       args = arguments;
+
+    //clear timeout will ignore the continious function call and only allows last function call which satisfy condition
     clearTimeout(timer);
     timer = setTimeout(() => {
-      fetchData.apply(context, args);
+      //apply is used because we are unaware about the function parameters. so to conquire this edge case we have used the apply
+      fn.apply(context, args);
     }, timeDiff);
   };
 }
